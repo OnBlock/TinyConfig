c5b3470e978868adfd8e6bc7306f5faf2e02a8e0
Indigo Amann <indigoa.coding@gmail.com>
2020-01-01 03:33:59 -0800

    Use defaults

--- src/main/java/io/github/indicode/fabric/tinyconfig/ConfigObject.java
+++ src/main/java/io/github/indicode/fabric/tinyconfig/ConfigObject.java
@@ -2,42 +2,116 @@ package io.github.indicode.fabric.tinyconfig;
 
 import java.util.function.Consumer;
 
-public interface ConfigObject<T> {
-    T get(String key);
-    T get(String key, T defaultValue);
-    T get(String key, T defaultValue, String comment);
-    
-    String getString(String key);
-    String getString(String key, String defaultValue);
-    String getString(String key, String defaultValue, String comment);
+public interface ConfigObject<T, C, A> {
+    default T get(String key) {
+        return get(key, (T) null);
+    }
+    default T get(String key, T defaultValue) {
+        return get(key, defaultValue, null);
+    }
+    default T get(String key, T defaultValue, String comment) {
+        return get(key, () -> defaultValue, comment);
+    }
+    default T get(String key, Getter<T> defaultValue) {
+        return get(key, defaultValue, null);
+    }
+    T get(String key, Getter<T> defaultValue, String comment);
 
-    int getInt(String key);
-    int getInt(String key, int defaultValue);
-    int getInt(String key, int defaultValue, String comment);
+    default String getString(String key) {
+        return getString(key, (String) null);
+    }
+    default String getString(String key, String defaultValue) {
+        return getString(key, defaultValue, null);
+    }
+    default String getString(String key, String defaultValue, String comment) {
+        return getString(key, () -> defaultValue, comment);
+    }
+    default String getString(String key, Getter<String> defaultValue) {
+        return getString(key, defaultValue, null);
+    }
+    String getString(String key, Getter<String> defaultValue, String comment);
 
-    double getDouble(String key);
-    double getDouble(String key, double defaultValue);
-    double getDouble(String key, double defaultValue, String comment);
+    default boolean getBool(String key) {
+        return getBool(key, false);
+    }
+    default boolean getBool(String key, boolean defaultValue) {
+        return getBool(key, defaultValue, null);
+    }
+    default boolean getBool(String key, boolean defaultValue, String comment) {
+        return getBool(key, () -> defaultValue, comment);
+    }
+    default boolean getBool(String key, Getter<Boolean> defaultValue) {
+        return getBool(key, defaultValue, null);
+    }
+    boolean getBool(String key, Getter<Boolean> defaultValue, String comment);
 
-    ConfigArray<T> getArray(String key);
-    ConfigArray<T> getArray(String key, ConfigArray<T> defaultValue);
-    ConfigArray<T> getArray(String key, ConfigArray<T> defaultValue, String comment);
+    default int getInt(String key) {
+        return getInt(key, 0);
+    }
+    default int getInt(String key, int defaultValue) {
+        return getInt(key, defaultValue, null);
+    }
+    default int getInt(String key, int defaultValue, String comment) {
+        return getInt(key, () -> defaultValue, comment);
+    }
+    default int getInt(String key, Getter<Integer> defaultValue) {
+        return getInt(key, defaultValue, null);
+    }
+    int getInt(String key, Getter<Integer> defaultValue, String comment);
 
-    void set(String key, T value);
+    default double getDouble(String key) {
+        return getDouble(key, 0);
+    }
+    default double getDouble(String key, double defaultValue) {
+        return getDouble(key, defaultValue, null);
+    }
+    default double getDouble(String key, double defaultValue, String comment) {
+        return getDouble(key, () -> defaultValue, comment);
+    }
+    default double getDouble(String key, Getter<Double> defaultValue) {
+        return getDouble(key, defaultValue, null);
+    }
+    double getDouble(String key, Getter<Double> defaultValue, String comment);
+
+    default A getArray(String key) {
+        return getArray(key, (A) null);
+    }
+    default A getArray(String key, A defaultValue) {
+        return getArray(key, defaultValue, null);
+    }
+    default A getArray(String key, A defaultValue, String comment) {
+        return getArray(key, () -> defaultValue, comment);
+    }
+    default A getArray(String key, Getter<A> defaultValue) {
+        return getArray(key, defaultValue, null);
+    }
+    A getArray(String key, Getter<A> defaultValue, String comment);
+
+    default void set(String key, T value) {
+        set(key, value, null);
+    }
     void set(String key, T value, String comment);
 
-    void setString(String key, String value);
+    default void setString(String key, String value) {
+        setString(key, value, null);
+    }
     void setString(String key, String value, String comment);
 
-    void setInt(String key, int value);
+    default void setInt(String key, int value) {
+        setInt(key, value, null);
+    }
     void setInt(String key, int value, String comment);
 
-    void setDouble(String key, double value);
+    default void setDouble(String key, double value) {
+        setDouble(key, value, null);
+    }
     void setDouble(String key, double value, String comment);
 
-    void setArray(String key, ConfigArray<T> value);
-    void setArray(String key, ConfigArray<T> value, String comment);
+    default void setArray(String key, A value) {
+        setArray(key, value, null);
+    }
+    void setArray(String key, A value, String comment);
 
-    void modifyChild(String key, Consumer<ConfigObject<T>> modifier);
-    void modifyChild(String key, Consumer<ConfigObject<T>> modifier, String comment);
+    void modifyChild(String key, Consumer<C> modifier);
+    void modifyChild(String key, Consumer<C> modifier, String comment);
 }
